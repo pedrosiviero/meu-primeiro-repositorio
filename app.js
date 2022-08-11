14dc43b7897e90361b5044ad83b614cd266b45bd
Larissa Vitto <110945122+Xaspovisk@users.noreply.github.com>
2022-08-10 21:21:52 -0300

    Adicionando mais uma linha em app.js

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
 console.log("Rodando o sistema e programação de aplicativos") 
 console.log("Rodando o sistema e programação de aplicativos 2") 
+console.log("Rodando o sistema e programação de aplicativos 3")
